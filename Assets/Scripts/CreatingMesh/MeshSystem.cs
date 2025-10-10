4d0611884644fdb2a59823db4caf81df6a49b59b
BejPastan <s.synaszko@gmail.com>
2025-10-10 20:13:40 +0200

    MeshJobs
    Creating strand Mesh using Jobs

--- Assets/Scripts/CreatingMesh/MeshSystem.cs
+++ Assets/Scripts/CreatingMesh/MeshSystem.cs
@@ -1,11 +1,14 @@
+using System;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.Rendering;
+using static UnityEngine.Rendering.HableCurve;
 
 [BurstCompile]
 [UpdateInGroup(typeof(InitializationSystemGroup))]
@@ -24,55 +27,76 @@ public partial struct StrandSpawner : ISystem
         int vertexCount = rings * vertexPerSegment;
 
         Mesh newMesh = new Mesh();
-        Mesh.MeshDataArray meshData = Mesh.AllocateWritableMeshData(1);
-        Mesh.MeshData data = meshData[0];
-        data.SetVertexBufferParams(vertexCount, new VertexAttributeDescriptor(VertexAttribute.Position));
-        var positions = data.GetVertexData<float3>();
 
-        for (int i = 0; i < rings; i++)
-        {
-            float yPos = (height / segments) * i;
-            positions[i * 4 + 1] = new float3(-baseSize / 2, yPos, -baseSize / 2);
-            positions[i * 4 + 0] = new float3(baseSize / 2, yPos, -baseSize / 2);
-            positions[i * 4 + 2] = new float3(baseSize / 2, yPos, baseSize / 2);
-            positions[i * 4 + 3] = new float3(-baseSize / 2, yPos, baseSize / 2);
-        }
+        GenerateVertices generateVertices = new GenerateVertices { baseSize = baseSize, segments = segments, vertices = new NativeArray<float3>(vertexPerSegment * (segments+1), Allocator.TempJob) };
+        var verticesHandle = generateVertices.Schedule();
+        verticesHandle.Complete();
 
-        int quads = segments * 4;
-        int indexCount = quads * 6;
+        
+        GenerateIndices generateIndices = new GenerateIndices{ segments = segments, indices = new NativeArray<ushort>(segments * 4 * 6, Allocator.TempJob)};
+        var indexHandle = generateIndices.Schedule();
+        indexHandle.Complete();
 
-        data.SetIndexBufferParams(indexCount, IndexFormat.UInt16);
-        var indices = data.GetIndexData<ushort>();
+        newMesh.SetVertices(generateVertices.vertices);
+        newMesh.SetIndices(generateIndices.indices, MeshTopology.Triangles, 0);
+        newMesh.RecalculateNormals();
 
-        int index = 0;
+        return newMesh;
+    }
+}
 
+[BurstCompile]
+public struct GenerateIndices : IJob
+{
+    const int vertexPerSegment = 4;
+    public int segments;
+    [WriteOnly]
+    public NativeArray<ushort> indices;
+
+    public void Execute()
+    {
         for (int i = 0; i < segments; i++)
         {
             int vStart = i * vertexPerSegment;
             //iterate over all sides of segment
-            for(int side = 0; side <4; side++)
+            for (int side = 0; side < 4; side++)
             {
-                int next = (side +1) % 4;
+                int next = (side + 1) % 4;
                 ushort v0 = (ushort)(vStart + side);
                 ushort v1 = (ushort)(vStart + next);
                 ushort v2 = (ushort)(vStart + side + vertexPerSegment);
                 ushort v3 = (ushort)(vStart + next + vertexPerSegment);
 
-                indices[index++] = v0;
-                indices[index++] = v2;
-                indices[index++] = v1;
-                indices[index++] = v1;
-                indices[index++] = v2;
-                indices[index++] = v3;
+                indices[i*6] = v0;
+                indices[i*6+1] = v2;
+                indices[i*6+2] = v1;
+                indices[i*6+3] = v1;
+                indices[i*6+4] = v2;
+                indices[i*6+5] = v3;
             }
         }
+    }
+}
 
-        data.subMeshCount = 1;
-        data.SetSubMesh(0, new SubMeshDescriptor(0, indexCount));
+[BurstCompile]
+public struct GenerateVertices : IJob
+{
+    const int vertexPerSegment = 4;
+    [ReadOnly]
+    public int segments;
+    [ReadOnly]
+    public float baseSize;
+    [WriteOnly]
+    public NativeArray<float3> vertices;
 
-        Mesh.ApplyAndDisposeWritableMeshData(meshData, newMesh);
-        newMesh.RecalculateNormals();
-
-        return newMesh;
+    public void Execute()
+    {
+        for (int i = 0; i < segments + 1; i++)
+        {
+            vertices[i * 4 + 1] = new float3(-baseSize / 2, i, -baseSize / 2);
+            vertices[i * 4 + 0] = new float3(baseSize / 2, i, -baseSize / 2);
+            vertices[i * 4 + 2] = new float3(baseSize / 2, i, baseSize / 2);
+            vertices[i * 4 + 3] = new float3(-baseSize / 2, i, baseSize / 2);
+        }
     }
 }
